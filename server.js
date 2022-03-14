b2a10036fa720bd065b58d7160cedc6e2db70c75
woffordlm <lukewofford@gmail.com>
2022-03-13 21:41:42 -0400

    merging into develop

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -8,5 +8,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(require('./routes'));
+
+
+//  mongoose.connect() tells Mongoose which database we want to connect to. 
+// If the environment variable MONGODB_URI exists, like on Heroku when we deploy later, 
+// it will use that. Otherwise, it will short-circuit to the local MongoDB server's database at mongodb://localhost:27017/pizza-hunt.
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
